762e0594776f5f28a74a86e9f6e0800b4302a0a4
DubDub Agent <dubdub@doublettactical.com>
2026-04-07 21:46:34 +0000

    suppress Chrome beforeunload deprecation warning from wouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Suppress Chrome deprecation warning from wouter's beforeunload handler
+window.addEventListener("unload", () => {});
+
 createRoot(document.getElementById("root")!).render(<App />);
